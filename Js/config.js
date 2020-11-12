1cd3e9307d9f563590e49ae567cb60211fab2102
prajyotrh <52719027+prajyotrh@users.noreply.github.com>
2020-11-12 11:45:03 +0530

    Add files via upload

---
+++ Js/config.js
@@ -0,0 +1,13 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDed0Q_kV_m2dqYjo5mcxRgtELN6WDnM8M",
+    authDomain: "todoapp-d01ff.firebaseapp.com",
+    databaseURL: "https://todoapp-d01ff.firebaseio.com",
+    projectId: "todoapp-d01ff",
+    storageBucket: "todoapp-d01ff.appspot.com",
+    messagingSenderId: "699402178665",
+    appId: "1:699402178665:web:8e64a44989ae40f80652f8",
+    measurementId: "G-Q3HV5SMZL6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
